ee076d37274e30dc16fe5983c1977db8b35daff7
Y4h14 <yahyasadig2013@gmail.com>
2024-06-10 16:43:06 +0300

    task 4 initial attempt

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""defines an asynchronous function"""
+from typing import List
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """returns a list of random values"""
+    delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
+    return delays
