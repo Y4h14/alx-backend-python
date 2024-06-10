be27ce2bcb9560c87f4e05ff68b70d6b213e998c
Y4h14 <yahyasadig2013@gmail.com>
2024-06-10 11:17:24 +0300

    changed the random fucntion to uniform

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,6 +6,6 @@ import asyncio
 
 async def wait_random(max_delay: int = 10) -> int:
     """returns a random wait time"""
-    i = random.randint(0, max_delay)
+    i = random.uniform(0, max_delay)
     await asyncio.sleep(i)
     return i
