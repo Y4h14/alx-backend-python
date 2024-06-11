3921adf3bd932548825bc796e4f2def3b5e281b0
Y4h14 <yahyasadig2013@gmail.com>
2024-06-11 16:07:49 +0300

    added sleep to task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,4 +8,5 @@ from typing import AsyncGenerator
 async def async_generator() -> AsyncGenerator:
     """ a coroutine that yeild a random numbeir"""
     for i in range(10):
+        await asyncio.sleep(1)
         yield random.uniform(0, 10)
