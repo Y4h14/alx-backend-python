042bf14686aa47672e6c1519af4cc4c88f803116
Y4h14 <yahyasadig2013@gmail.com>
2024-07-01 23:51:10 +0300

    attempt at task 6

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -13,7 +13,7 @@ class TestGithubOrgClient(unittest.TestCase):
         ("google", {'login': "google"}),
         ("abc",  {'login': "abc"})
     ])
-    @patch('get_json')
+    @patch('utils.get_json')
     def test_org(self, org, resp, mock_func):
         mock_func.return_value = MagicMock(return_value=resp)
         org_client = GithubOrgClient(org)
@@ -23,7 +23,7 @@ class TestGithubOrgClient(unittest.TestCase):
             )
 
     def test_public_repos_url(self):
-        with patch("GithubOrgClient.org",
+        with patch("client.GithubOrgClient.org",
                    new_callable=PropertyMock) as mock:
             mock.return_value = {
                 'repos_url': "https://api.github.com/users/google/repos",
@@ -32,3 +32,39 @@ class TestGithubOrgClient(unittest.TestCase):
                 GithubOrgClient('google')._public_repos_url,
                 "https://api.github.com/users/google/repos"
             )
+
+    @patch('utils.get_json')
+    def test_public_repos(self, get_josn_mock):
+        test_payload = {
+            'repos_url': "https://api.github.com/users/facebook/repos",
+            'repos' :[
+                {
+                    "id": 381489928,
+                    "node_id": "MDEwOlJlcG9zaXRvcnkzODE0ODk5Mjg=",
+                    "name": "akd",
+                    "full_name": "facebook/akd",
+                    "private": False,
+                    "owner": {
+                      "login": "facebook",
+                      "id": 69631,
+                }},
+                {
+                    "id": 381489928,
+                    "node_id": "MDEwOlJlcG9zaXRvcnkzODE0ODk5Mjg=",
+                    "name": "akd",
+                    "full_name": "facebook/akd",
+                    "private": False,
+                    "owner": {
+                      "login": "facebook",
+                      "id": 69631,
+                }}]}
+    
+        get_josn_mock.return_value = test_payload['repos']
+        with patch("client.GithubOrgClient._public_repos_url",
+                   new_callable=PropertyMock ) as mock:
+            mock.return_value = test_payload["repos_url"]
+            self.assertEqual(
+                GithubOrgClient('facebook').public_repos(),
+                ["akd", "akd"])
+            mock.assert_called_once()
+        get_josn_mock.assert_called_once()
