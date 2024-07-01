22b95996eeb48b1c7de0ba5c7e4805a54d3f8424
Y4h14 <yahyasadig2013@gmail.com>
2024-07-01 17:46:27 +0300

    updated task 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -39,7 +39,7 @@ class TestGetJson(unittest.TestCase):
             req.assert_called_once_with(test_url)
 
 
-class  TestMemoize(unittest.TestCase):
+class TestMemoize(unittest.TestCase):
     """a unit test class for testing utils memoize"""
 
     def test_memoize(self):
@@ -52,7 +52,8 @@ class  TestMemoize(unittest.TestCase):
             def a_property(self):
                 return self.a_method()
 
-        with patch.object(TestClass, "a_method", return_value=lambda: 42,) as memo:
+        with patch.object(TestClass, "a_method",
+                          return_value=lambda: 42,) as memo:
             test_class = TestClass()
             self.assertEqual(test_class.a_property(), 42)
             self.assertEqual(test_class.a_property(), 42)
